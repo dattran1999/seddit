a7fdd06c18c607e2a5c2abb384ac53121b7b1594
dattran1999 <z5210945@student.unsw.edu.au>
2019-07-30 23:14:35 +1000

    Authenticator works

--- frontend/src/loginPage.js
+++ frontend/src/loginPage.js
@@ -1,29 +1,44 @@
-import {addChildrenToElement, createInput} from './utils.js'
+import {addChildrenToElement, createInput, createNewElement} from './utils.js'
 
-export default function renderLoginPage() {
+export default function renderLoginPage(apiUrl) {
     let mainContent = document.getElementsByTagName('main')[0];
-    const feed = mainContent.innerHTML;
     mainContent.innerText = "";
     // create login form, with a div wrapped around it
     let loginForm = document.createElement('form');
-    let loginFormDiv = document.createElement('div');
+    loginForm.classList.add("auth-form")
+    
     // create input fields
     let usernameInput = createInput('text', 'username', 'Enter Username');
     let passwordInput = createInput('password', 'password', 'Enter Password');
-    let submitButton = document.createElement('button');
-    submitButton.innerText = 'SUBMIT';
-    submitButton.setAttribute('value', 'Submit');
-    submitButton.classList.add('button','button-secondary');
+    let submitButton = createNewElement('button', {'value': 'Submit', "class": 'button button-secondary'}, 'SUBMIT')
+    
+    // put all input into the form
+    addChildrenToElement(loginForm, usernameInput, passwordInput, submitButton);
+    mainContent.appendChild(loginForm);
 
     submitButton.addEventListener('click', (e) => {
         e.preventDefault()
-        console.log('clicked submit');
+        const option = {
+            body: `{ "username": "${usernameInput.value}", "password": "${passwordInput.value}"}`,
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            method: "POST"
+        }
+        fetch(`${apiUrl}/auth/login`, option)
+            .then(res => {
+                if (res !== 200) {
+                    throw Error('Wrong username or password')
+                }
+                res.json()
+            })
+            .catch(res => {
+                let errorMsg = createNewElement('p', {"class": 'error-message'}, res)
+                loginForm.insertBefore(errorMsg, loginForm.childNodes[2]);
+            })
+            .then(res => {
+                // TODO: fetch posts of the user
+            })
     })
-    // put all input into the form
-    addChildrenToElement(loginForm, usernameInput, passwordInput, submitButton);
-    loginFormDiv.appendChild(loginForm);
-    loginFormDiv.setAttribute('class', 'form')
-    console.log(loginFormDiv);
-    mainContent.appendChild(loginFormDiv);
-    return feed;
 }
