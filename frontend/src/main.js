fd2fe4c014b108208a0641398c496a123ec870cf
dattran1999 <z5210945@student.unsw.edu.au>
2019-08-12 15:57:04 +1000

    Added template html in main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,11 +9,13 @@ import renderLoginPage from './loginPage.js'
 import renderSignupPage from './signupPage.js'
 import renderNewsFeed from './newsFeed.js'
 import renderNavBar from "./components/navbar.js"
-import {addChildrenToElement} from './utils.js'
+import {addChildrenToElement, createNewElement} from './utils.js'
 // your app must take an apiUrl as an argument --
 // this will allow us to verify your apps behaviour with 
 // different datasets.
 function initApp(apiUrl) {
+    createTemplateHtml();
+    
     renderNavBar();
     const token = localStorage.getItem('sedditToken');
     if (token !== null) {
@@ -38,4 +40,29 @@ function initApp(apiUrl) {
     });
 }
 
+function createTemplateHtml() {
+    const root = document.getElementById('root');
+    root.appendChild(createHeaderHtml());
+    let main = createNewElement('main', {"role": 'main'})
+    root.appendChild(main);
+    console.log(root)
+}
+
+function createHeaderHtml() {
+    let header = createNewElement('header', {"class": "banner", "id": "nav"})
+    let logo = createNewElement('h1', {"id": "logo", "class": "flex-center"}, "Seddit")
+    let navbar = createNewElement('ul', {'class': 'nav'})
+    let navItem0 = createNewElement('li', {'class': 'nav-item'})
+    let searchBar = createNewElement('input', {'id': "search", 'data-id-search': "", "placeholder": "Search Seddit", "type": "search"})
+    navItem0.appendChild(searchBar)
+    let navItem1 = createNewElement('li', {'class': 'nav-item'})
+    let loginButton = createNewElement('button', {"class":"button button-primary", "data-id-login": ""}, "LOGIN");
+    navItem1.appendChild(loginButton)
+    let navItem2 = createNewElement('li', {'class': 'nav-item'})
+    let signupButton = createNewElement('button', {"class":"button button-secondary", "data-id-signup": ""}, "SIGN UP");
+    navItem2.appendChild(signupButton)
+    addChildrenToElement(navbar, navItem0, navItem1, navItem2)
+    addChildrenToElement(header, logo, navbar)
+    return header;
+}
 export default initApp;
